4c32aa1fe132112afe7f0666b01b56e91b2077e8
Antonia <antonianomamiukor@gmail.com>
2020-04-24 11:36:43 -0700

    setup requirements, created board with specific name

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ const App = () => {
       </header>
       <Board
         url="https://inspiration-board.herokuapp.com/boards/"
-        boardName={`Ada-Lovelace`}
+        boardName={`nora-antonia`}
       />
     </section>
   );
