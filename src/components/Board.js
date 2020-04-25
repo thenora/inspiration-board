086cb3d07cf92d77aab128e809c4e15d7e59b69d
thenora <thenora@gmail.com>
2020-04-25 15:21:08 -0700

    started API and useEffect on Board

--- src/components/Board.js
+++ src/components/Board.js
@@ -1,22 +1,48 @@
-import React, { Component } from 'react';
+import React, { Component, useState, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import axios from 'axios';
-
+​
 import './Board.css';
 import Card from './Card';
 import NewCardForm from './NewCardForm';
-import CARD_DATA from '../data/card-data.json';
-
+// import CARD_DATA from '../data/card-data.json';
+const BASE_URL = 'https://inspiration-board.herokuapp.com/boards/nora-antonia';
+​
 const Board = () => {
-  const cards = CARD_DATA.cards.map ((card) => {
-    return  (
-      <Card 
+  
+  // const cards = CARD_DATA.cards.map ((card) => {
+  //   return  (
+  //     <Card 
+  //       text={card.text} 
+  //       emoji={card.emoji}
+  //     />
+  //   )
+  // })
+  const [cards, setCards] = useState([]);
+  const [errorMessage, setErrorMessage] = useState(null);
+    useEffect(() => {
+      axios.get(BASE_URL + '/cards')
+      .then((response) => {
+        // Get the list of students
+      const boardCards = response.data.map ((card, i) => {
+      return  (
+      <Card
+        id = {i}
         text={card.text} 
         emoji={card.emoji}
       />
     )
   })
-
+        
+        setCards(boardCards)
+      })
+      .catch((error) => {
+        // Still need to handle errors
+        setErrorMessage(error.message)
+      });
+  }, []);
+​
+​
   return (
     <div className="board">
       {cards}
@@ -24,7 +50,7 @@ const Board = () => {
   )
 };
 Board.propTypes = {
-
+​
 };
-
+​
 export default Board;
