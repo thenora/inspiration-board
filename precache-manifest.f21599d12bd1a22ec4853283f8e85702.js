37b2b9f9147601dc79bb60ec559f7ecc651ef497
thenora <thenora@gmail.com>
2020-04-26 22:37:23 -0700

    Updates

---
+++ precache-manifest.f21599d12bd1a22ec4853283f8e85702.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d30819b8eda840faeb78450b58761174",
+    "url": "/inspiration-board/index.html"
+  },
+  {
+    "revision": "ffe3071d05286a06d39a",
+    "url": "/inspiration-board/static/css/main.9a622717.chunk.css"
+  },
+  {
+    "revision": "4206493d4c4cd5a1bbc6",
+    "url": "/inspiration-board/static/js/2.67d02754.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/inspiration-board/static/js/2.67d02754.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ffe3071d05286a06d39a",
+    "url": "/inspiration-board/static/js/main.c356b926.chunk.js"
+  },
+  {
+    "revision": "00bdabaa7c63b64db8f0",
+    "url": "/inspiration-board/static/js/runtime-main.b2c7c5f9.js"
+  }
+]);
